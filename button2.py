0f3ade21ed45f30768aaec54151cff0cf652121a
DongJae Yoon <paipias3@naver.com>
2017-11-14 21:35:54 -0800

    name and student-id

---
+++ button2.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window =Tk()
+
+b1 = Button(window, text="DongJae Yoon")
+b2 = Button(window, text="60122775")
+b3 = Button(window, text="0618")
+b1.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+b3.grid(row=0, column=1)
+
+window.mainloop()
